761143d04c099ec957c4562d92b9eef914b4b669
harshbhagtani <harsh.bhagtani1@gmail.com>
2024-06-07 22:19:47 +0530

    added ping page

---
+++ src/pages/health/ping/index.tsx
@@ -0,0 +1,8 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+const Health = () => {
+  return <Typography sx={{ color: "black" }}>Health check</Typography>;
+};
+
+export default Health;
