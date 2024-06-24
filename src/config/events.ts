a1572e9c20743d91a425ecda5b05ca87c826c79d
Arun-chaitanya <arunchaitanya.iitkgp@gmail.com>
2024-06-24 20:15:54 +0530

    header and footer events added

---
+++ src/config/events.ts
@@ -0,0 +1,22 @@
+const HEADER_EVENTS = {
+    HEADER_LOGO_CLICKED: "header_logo_clicked",
+    HEADER_ABOUT_US_NAVIGATION_CLICKED: "header_about_us_navigation_clicked",
+    HEADER_PARTNER_NAVIGATION_CLICKED: "header_partner_navigation_clicked",
+    HEADER_LOGIN_CLICKED: "header_login_clicked"
+}
+
+const FOOTER_EVENTS = {
+    FOOTER_LOGO_CLICKED: "footer_logo_clicked",
+    FOOTER_ABOUT_US_CLICKED: "footer_about_us_clicked",
+    FOOTER_PARTNER_CLICKED: "footer_partner_clicked",
+    FOOTER_CONTACT_US_CLICKED: "footer_contact_us_clicked",
+    FOOTER_SOCIALS_CLICKED: "footer_socials_clicked"
+}
+
+const FOOTER_SOCIALS_TYPE = {
+    FACEBOOK: "Facebook",
+    TWITTER: "Twitter",
+    LINKEDIN: "LinkedIn",
+    YOUTUBE: "Youtube",
+    INSTAGRAM: "Instagram"
+}
