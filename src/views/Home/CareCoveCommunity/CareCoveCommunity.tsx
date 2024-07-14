a4a7b9077a0811c1d1e8d50b6ed86cade702fed1
Arun Chaitanya <arunchaitanya.iitkgp@gmail.com>
2024-07-15 01:43:25 +0530

    added home carousel section

--- src/views/Home/CareCoveCommunity/CareCoveCommunity.tsx
+++ src/views/Home/CareCoveCommunity/CareCoveCommunity.tsx
@@ -5,34 +5,38 @@ import PageTitle from "@components/PageTitle";
 import Text from "@components/Text";
 import Carousel from "@components/Carousel";
 import styles from "./CareCoveCommunity.module.scss";
+import useBreakpoint from "@hooks/useBreakpoint";
+
+const Testimonial: React.FC<{ key: number }> = ({ key }) => {
+  const isMobile = useBreakpoint({ max: "sm" });
+  return (
+    <div className={styles.testimonial} key={key}>
+      <Image src={"./assets/coreteam/steve.png"} alt="Testimonial" className={styles.image} />
+
+      <Text align="center" weight="medium" color="light" size="lg" className={styles.quote}>
+        “I wouldn’t be in the position I am today if it wasn’t for the support I received from Jackie. I started
+        {/* eslint-disable-next-line react/no-unescaped-entities */}
+        Carecove because it sucks to be a “Jackie” but it's SO important and I think little bits of support from friends
+        can go a long way!”.
+      </Text>
+      <Text align="center" weight="bold" color="light" size={isMobile ? "lg" : "xl"}>
+        Stephen Lane
+      </Text>
+      <Text align="center" weight="bold" color="light" size={isMobile ? "lg" : "xl"}>
+        Executive Chairman & Co -Founder
+      </Text>
+    </div>
+  );
+};
 
 const CareCoveCommunity: React.FC = () => {
-  const Testimonial: React.FC<{ key: number }> = ({ key }) => {
-    return (
-      <div className={styles.testimonial} key={key}>
-        <Image src={"./assets/image-placeholder-testimonial.svg"} alt="Testimonial" className={styles.image} />
-        <Text align="center" weight="medium" color="light" font="primary" size="lg" className={styles.receipt}>
-          Jane received $142.51 from 62 boosts, thanks to her Carecove Community!
-        </Text>
-        <Text align="center" weight="medium" color="light" font="primary" size="lg" className={styles.quote}>
-          “Carecove was exactly the community and platform I needed when I became a caregiver for my sister after a
-          tragic accident. It provided cashback when I needed it most and enabled my family and friends to support us
-          effectively. Carecove has been a lifeline during this challenging time”.
-        </Text>
-        <Text align="center" weight="bold" color="light" font="primary" size="lg" className={styles.author}>
-          Jane Davis: Carecove Member
-        </Text>
-      </div>
-    );
-  };
-
   const slides = [<Testimonial key={1} />, <Testimonial key={2} />, <Testimonial key={3} />];
 
   return (
     <section className={styles.careCoveCommunity}>
       <Container>
         <PageTitle align="center" color="radical-plum" className="mb60">
-          Hear From Our Carecove Community
+          Hear From Our Carecove Team
         </PageTitle>
         <div className={styles.carouselContainer}>
           <Carousel components={slides} className={styles.carousel} />
