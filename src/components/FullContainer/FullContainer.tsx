61a57117b08b9380889c2c8cb16b6dbe21e54704
Arun Chaitanya <arunchaitanya.iitkgp@gmail.com>
2024-07-25 12:32:37 +0530

    changing fullcontainer from container to div

--- src/components/FullContainer/FullContainer.tsx
+++ src/components/FullContainer/FullContainer.tsx
@@ -1,11 +1,9 @@
 import clsx from "clsx";
 
-import { Container } from "@mui/material";
-
 import styles from "./FullContainer.module.scss";
 
 const FullContainer: React.FC<FullContainerProps> = ({ children, className }) => {
-  return <Container className={clsx(styles.root, className)}>{children}</Container>;
+  return <div className={clsx(styles.root, className)}>{children}</div>;
 };
 
 type FullContainerProps = React.PropsWithChildren<{
